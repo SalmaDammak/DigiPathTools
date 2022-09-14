c36faf64ec0cbd98aed220a03f4b49122d7d1749
Salma Dammak <sdammak@uwo.ca>
2022-09-14 12:37:52 -0400

    New function to prepare a table to be classified

--- Utils/TilesTableUtils.m
+++ Utils/TilesTableUtils.m
@@ -3,13 +3,29 @@ classdef TilesTableUtils
     %
     % A collection of uttilities to prepare and modify the tables
     % containing tile information in the format my python code currently
-    % requires it to be. 
+    % requires it to be.
     
     % Primary Author: Salma Dammak
     % Created: Jun 22, 2022
     
-   methods (Static)
-       
-   end
+    methods (Static)
+        
+        function tTiles = MakeTableFromSlideFolder(sTileAndLabelmapDir, chTargetCSVFilepath)
+            arguments
+                sTileAndLabelmapDir (1,1) string {mustBeText,...
+                    MyValidationUtils.MustBeExistingDir,...
+                    MyValidationUtils.MustBeDirPath,...
+                    MyValidationUtils.MustBeNonEmptyDir}
+                chTargetCSVFilepath
+            end
+            
+            stTileFilePaths = dir(fullfile(sTileAndLabelmapDir, QuPathUtils.sImageRegexp));            
+            tTiles = string({stTileFilePaths.name}');            
+            
+            writecell({stTileFilePaths.name}',chTargetCSVFilepath, 'FileType', 'text', 'delimiter',',');
+            
+        end
+        
+    end
 end
 
